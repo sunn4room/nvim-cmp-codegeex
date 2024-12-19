0a4ad333557bc427edef84f0673e0bbe76aeb520
sunn4room <sunn4room@163.com>
2024-12-19 10:18:56 +0800

    fix: immediately interrupt

--- lua/cmp-codegeex.lua
+++ lua/cmp-codegeex.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 M.setup = function(opts)
-  local source = { count = 0 }
+  local source = {
+    interrupt = function() end,
+    timer = vim.uv.new_timer(),
+  }
 
   function source:is_available()
     if not vim.g.use_codegeex then
@@ -31,18 +34,11 @@ M.setup = function(opts)
   end
 
   function source:complete(request, callback)
-    self.count = self.count + 1
-    local id = self.count
-
-    vim.defer_fn(function()
-      if id ~= self.count then
-        callback()
-        return
-      end
-
+    self.interrupt()
+    self.timer:start(opts.delay or 500, 0, vim.schedule_wrap(function()
       local prompt = string.sub(request.context.cursor_before_line, request.offset)
       local path = vim.fn.expand "%"
-      local language = vim.api.nvim_buf_get_option(0, "filetype")
+      local language = vim.api.nvim_get_option_value("filetype", { buf = 0 })
       local cursor = { request.context.cursor.row - 1, request.context.cursor.col - 1 }
       local range = {
         math.max(0, cursor[1] - (opts.range or 500)),
@@ -58,22 +54,25 @@ M.setup = function(opts)
           spinner = spinner == #spinner_frames and 1 or spinner + 1
           callback {
             isIncomplete = true,
-            items = {{
-              label = prompt .. " " .. spinner_frames[spinner],
-              insertText = prompt,
-              filterText = prompt,
-              cmp = {
-                kind_text = "CodeGeeX",
-                kind_hl_group = "CmpItemKindCodeGeeX",
+            items = {
+              {
+                label = prompt .. " " .. spinner_frames[spinner],
+                insertText = prompt,
+                filterText = prompt,
+                cmp = {
+                  kind_text = "CodeGeeX",
+                  kind_hl_group = "CmpItemKindCodeGeeX",
+                },
               },
-            }},
+            },
           }
           vim.defer_fn(function() update_spinner() end, 100)
         end
       end
       update_spinner()
 
-      vim.system(
+      local killed = false
+      local process = vim.system(
         {
           "curl",
           "--location",
@@ -98,22 +97,16 @@ M.setup = function(opts)
         },
         { text = true },
         vim.schedule_wrap(function(result)
+          if killed then return end
           spinner = 0
-          if id ~= self.count then
-            callback()
-            return
-          end
-
-          if result.code == 0 and result.signal == 0 then
-            local result_obj = vim.fn.json_decode(result.stdout)
-            if result_obj.error then
-              vim.notify(result_obj.error.message, 3, { title = "CodeGeeX" })
+          if result.code == 0 then
+            local response = vim.fn.json_decode(result.stdout)
+            if response.error then
+              vim.notify(response.error.message, 3, { title = "CodeGeeX" })
               callback()
             else
-              local choice = result_obj.choices[1]
+              local choice = response.choices[1]
               local content = choice.message.content
-              local first_cr = string.find(content .. "\n", "\n")
-              local first = string.sub(content, 1, first_cr - 1)
               local before = request.context.cursor_before_line
               local after = request.context.cursor_after_line
               if after == "" and content:sub(-1) == "\n" then
@@ -121,26 +114,40 @@ M.setup = function(opts)
               end
               callback {
                 isIncomplete = true,
-                items = {{
-                  label = prompt .. first,
-                  documentation = {
-                    kind = "markdown",
-                    value = "```txt\n" .. before .. content .. after .. "\n```",
+                items = {
+                  {
+                    label = prompt .. "…",
+                    insertText = prompt .. content,
+                    documentation = {
+                      kind = "markdown",
+                      value = "```txt\n" .. before .. content .. after .. "\n```",
+                    },
+                    cmp = {
+                      kind_text = "CodeGeeX",
+                      kind_hl_group = "CmpItemKindCodeGeeX",
+                    },
                   },
-                  cmp = {
-                    kind_text = "CodeGeeX",
-                    kind_hl_group = "CmpItemKindCodeGeeX",
-                  },
-                }},
+                },
               }
             end
           else
-            vim.notify("curl run failed!", 3, { title = "CodeGeeX" })
+            vim.notify("Request failed: " .. result.stderr, 3, { title = "CodeGeeX" })
             callback()
           end
+          self.interrupt = function() end
         end)
       )
-    end, opts.delay or 500)
+      self.interrupt = function()
+        process:kill(15)
+        killed = true
+        spinner = 0
+        callback { isIncomplete = true, items = {} }
+      end
+    end))
+    self.interrupt = function()
+      self.timer:stop()
+      callback { isIncomplete = true, items = {} }
+    end
   end
 
   require("cmp").register_source("codegeex", source)
